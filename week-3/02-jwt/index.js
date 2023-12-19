00d67930887e49ee6ebc85a6c22dbf9e082ede11
Xaheersays <shzaheer514@gmail.com>
2023-12-19 16:57:31 +0530

    jwt assignment done

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,17 +1,47 @@
 const jwt = require('jsonwebtoken');
+const zod = require('zod')
 const jwtPassword = "secret";
 
+const validateInput = zod.object({
+    username :zod.string().email(),
+    password :zod.string().min(6)
+})
+
 function signJwt(username, password) {
+    const user = {username:username,password:password}
+    const validataion = validateInput.safeParse(user)
+    if (!validataion.success){
+        return null;
+    }
+    // console.log(validataion)
+    const token = jwt.sign(user,jwtPassword);
+    // console.log(token)
+    return token;
 
 }
 
 function verifyJwt(token) {
+    try{
+        const verifiedTokenResult = jwt.verify(token,jwtPassword);
+        console.log(verifiedTokenResult)
+        return true;
+    }catch(err){
+        return false;
+    }
 
 }
 
 function decodeJwt(token) {
-
+    const parts = token.split('.')
+    if (!(parts.length===3))return false
+    try{
+        const results  = jwt.decode(token );
+        return true;
+    }catch(err){
+        return false;
+    }
 }
+
 
 module.exports = {
     signJwt,
